af53b047e7ba502b99240deec79ecca56eb4c7b6
Julian <jcvgalfonso@gmail.com>
2015-08-11 20:58:39 -0700

    First commit of gallery app

--- gallery/settings.py
+++ gallery/settings.py
@@ -95,18 +95,18 @@ USE_TZ = True
 
 
 import dj_database_url
-DATABASES = {
-    'default': {
-#          'ENGINE': 'django.db.backends.mysql',
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+         #'ENGINE': 'django.db.backends.postgresql_psycopg2',
          
 #        'NAME': 'GALLERY_DB',
 #        'USER': 'b41da56df47d3f@us-cdbr-iron-east-02.cleardb.net',
 #        'PASSWORD': '1c9e6a96 ',
-    }
-}
+#    }
+#}
 #DATABASES = {'default': dj_database_url.config()}
-
+DATABASES = {'default': dj_database_url.config(default=os.environ['CLEARDB_DATABASE_URL'])}
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
